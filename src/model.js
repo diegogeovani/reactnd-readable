ffdb8c60acbb5639db4796d639e44936f5344f76
Diego Santos <diego.santos@olxbr.com>
2018-05-29 21:42:57 -0300

    Make the post form stateful

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const post = {
+  id: '',
+  timestamp: '',
+  title: '',
+  body: '',
+  author: '',
+  category: '',
+  voteScore: 0,
+  deleted: false
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { post } from '../model'
 import '../styles/MainPage.css'
 
-const MainPage = () => (
-  <div>
-    <header><h2>New post</h2></header>
-    <p>Describe the details below as much as possible</p>
-    <form className="form-post">
-      <input type="text" name="title" placeholder="Title" />
-      <input type="text" name="author" placeholder="Author" />
-      <select>
-        <option key="placeholder" value="placeholder">Category</option>
-      </select>
-      <textarea placeholder="Body" name="body" />
-      <button>Post</button>
-    </form>
-    <time>{new Date().toString()}</time>
-  </div>
-)
+class MainPage extends Component {
+
+  state = post
+
+  saveState = (target) => {
+    const newState = this.state
+    newState[target.name] = target.value
+    this.setState(newState)
+  }
+
+  onInput = (event) => this.saveState(event.target)
+
+  postSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  render() {
+    const state = this.state
+
+    return (
+      <div>
+        <header><h2>New post</h2></header>
+        <p>Describe the details below as much as possible</p>
+        <form onSubmit={this.postSubmit} className="form-post">
+          <input onChange={this.onInput} type="text" name="title" placeholder="Title" value={state.title} />
+          <input onChange={this.onInput} type="text" name="author" placeholder="Author" value={state.author} />
+          <select>
+            <option key="placeholder" value="placeholder">Category</option>
+          </select>
+          <textarea onChange={this.onInput} placeholder="Body" name="body" value={state.body} />
+          <button>Post</button>
+        </form>
+        <time>{new Date().toString()}</time>
+      </div>
+    )
+  }
+}
 
 export default MainPage
