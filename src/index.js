d6b7e2ada85990e09a7a31a9f112a28a1781c928
Diego Santos <diego.santos@olxbr.com>
2018-05-29 22:15:23 -0300

    Create a Redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
+const reducer = () => ({test: 'Hello, Redux store ;D'})
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>,
   document.getElementById('root')
 )
 registerServiceWorker();
