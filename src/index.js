97e3fbdcf2ce9bad9a17e67bc1f5d0e79d28ab6a
Diego Santos <diego.santos@olxbr.com>
2018-06-20 08:02:43 -0300

    Apply Redux Thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import reducer from './state/reducers/index'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
-  <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker();
