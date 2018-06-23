386d39e342ce000c9e4800735167bb8737345527
Diego Santos <diego.santos@olxbr.com>
2018-06-23 11:50:49 -0300

    Replace authentication with API

--- src/apis/readable.js
+++ src/apis/readable.js
@@ -2,7 +2,7 @@ const api = "http://localhost:3001"
 
 const headers = {
   'Accept': 'application/json',
-  'Authorization': 'Diego Geovani Andrade dos Santos'
+  'Authorization': 'Diego Santos'
 }
 
 export const getCategories = () =>
