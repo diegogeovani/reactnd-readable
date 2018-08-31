7d72adaa0b1d4043dfa7010a5b967a1d047a2a2b
Diego Santos <diego.santos@olxbr.com>
2018-08-31 16:53:22 -0300

    Clear comment form after its subimission

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const getInitialState = () => ({
+  author: '',
+  body: '',
+})
+
 class CommentForm extends Component {
 
   static propTypes = {
     onSubmit: PropTypes.func.isRequired,
   }
 
+  state = getInitialState()
+
   render() {
     const { onSubmit } = this.props
+    const { author, body } = this.state
 
     return (
-      <form onSubmit={onSubmit}>
-        <input type='author' name='author' placeholder='Author' />
-        <textarea name='body' placeholder='Write your comment here' />
+      <form onSubmit={(e) => {
+        onSubmit(e)
+        this.setState(getInitialState())
+      }}>
+        <input
+          type='text'
+          name='author'
+          placeholder='Author'
+          value={author}
+          onChange={(e) => this.setState({ author: e.target.value })} />
+        <textarea
+          name='body'
+          placeholder='Write your comment here'
+          value={body}
+          onChange={(e) => this.setState({ body: e.target.value })} />
         <button>Comment</button>
       </form>
     )
