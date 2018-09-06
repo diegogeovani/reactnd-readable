a28c7a9f6929b2877a99992de040fe73b0ce3275
Diego Santos <diego.santos@olxbr.com>
2018-09-06 09:29:24 -0300

    Connect React Router events to Redux store

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { createStore, compose, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { createBrowserHistory } from 'history'
+import {
+  createStore,
+  compose,
+  applyMiddleware
+} from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import {
+  connectRouter,
+  routerMiddleware,
+  ConnectedRouter
+} from 'connected-react-router'
+
 import reducer from './state/reducers/index'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+
 import './index.css'
+
+const history = createBrowserHistory()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(thunk))
+  connectRouter(history)(reducer),
+  composeEnhancers(
+    applyMiddleware(thunk),
+    applyMiddleware(routerMiddleware(history))
+  )
 )
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <ConnectedRouter history={history}>
       <App />
-    </BrowserRouter>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
