add8b5d099600fea8de068c8973dedeac77db078
Diego Santos <diego.santos@olxbr.com>
2018-09-10 07:06:15 -0300

    Show 404 page when path or resorce are not found

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Route, withRouter } from 'react-router-dom'
+import {
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom'
 import { connect } from 'react-redux'
 import queryString from 'query-string'
 
+import { fetchAll } from '../state/actions'
 import MainPage from '../pages/MainPage'
 import PostPage from '../pages/PostPage'
 import PostViewPage from '../pages/PostViewPage'
-
-import { fetchAll } from '../state/actions'
+import NotFoundPage from '../pages/NotFoundPage'
 
 import '../styles/App.css'
 
 class App extends Component {
 
   static propTypes = {
-    onInit: PropTypes.func.isRequired
+    onInit: PropTypes.func.isRequired,
+    categories: PropTypes.array,
+    posts: PropTypes.array,
   }
 
   componentDidMount() {
@@ -27,45 +33,67 @@ class App extends Component {
   )
 
   render() {
+    const { categories, posts } = this.props
+
     return (
       <div className="app">
-        <Route
-          exact path="/"
-          render={(props) => (
-            <MainPage sorting={this.parseSorting(props)} />
-          )} />
+        <Switch>
+          <Route
+            exact path="/"
+            render={(props) => (
+              <MainPage sorting={this.parseSorting(props)} />
+            )} />
 
-        <Route
-          exact path="/:path"
-          render={(props) => (props.match.params.path !== 'posts' ?
-            <MainPage
-              category={props.match.params.path}
-              sorting={this.parseSorting(props)} /> :
-            <PostPage />
-          )} />
+          <Route
+            exact path="/posts"
+            render={() => (<PostPage />)} />
 
-        <Route
-          path="/posts/:id/edit"
-          render={(props) => (
-            <PostPage
-              id={props.match.params.id} />
-          )} />
+          <Route
+            exact path="/:path"
+            render={(props) => (
+              categories && !!categories.find(c => c === props.match.params.path) ?
+                <MainPage
+                  category={props.match.params.path}
+                  sorting={this.parseSorting(props)} /> :
+                <NotFoundPage />
+            )} />
 
-        <Route
-          exact path='/:category/:post_id'
-          render={(props) => (
-            <PostViewPage id={props.match.params.post_id} />
-          )} />
+          <Route
+            path="/posts/:id/edit"
+            render={(props) => (
+              posts && !!posts.find(p => p.id === props.match.params.id) ?
+                <PostPage
+                  id={props.match.params.id} /> :
+                <NotFoundPage />
+            )} />
+
+          <Route
+            exact path='/:category/:post_id'
+            render={(props) => (
+              categories &&
+                posts &&
+                !!categories.find(c => c === props.match.params.category) &&
+                !!posts.find(p => p.id === props.match.params.post_id) ?
+                < PostViewPage id={props.match.params.post_id} /> :
+                <NotFoundPage />
+            )} />
+
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = (state) => ({
+  categories: Object.keys(state.categories),
+  posts: Object.values(state.posts).filter(p => !p.deleted)
+})
 
-function mapDispatchToProps(dispatch) {
-  return {
-    onInit: () => fetchAll()(dispatch)
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  onInit: () => fetchAll()(dispatch)
+})
 
-export default withRouter(connect(null, mapDispatchToProps)(App))
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(App)
+)
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => (
+  <main>
+    <h2>Page not found</h2>
+  </main>
+)
+
+export default NotFoundPage
